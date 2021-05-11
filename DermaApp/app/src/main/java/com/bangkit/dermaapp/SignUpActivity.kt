cb56c6cb60d49a6b26ae1a6c5029799b6c3a20f6
alzainiazharie <alzainiazharie@gmail.com>
2021-05-12 00:04:42 +0800

    layout : Login dan Sign Up, string udah semua (login sign up) , (color.xml belum semua)

---
+++ DermaApp/app/src/main/java/com/bangkit/dermaapp/SignUpActivity.kt
@@ -0,0 +1,11 @@
+package com.bangkit.dermaapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
