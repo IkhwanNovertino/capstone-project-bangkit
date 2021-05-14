b803a8b226c8c8ddf5611a808d15e9ad75d46f71
alzainiazharie <alzainiazharie@gmail.com>
2021-05-15 07:06:08 +0800

    improve ux login dan sign up

--- DermaApp/app/src/main/java/com/bangkit/dermaapp/MainActivity.kt
+++ DermaApp/app/src/main/java/com/bangkit/dermaapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.bangkit.dermaapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.Toast
 import com.bangkit.dermaapp.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -22,10 +23,23 @@ class MainActivity : AppCompatActivity() {
             val email = binding.edtEmail.text.trim().toString()
             val password = binding.edtPassoword.text.trim().toString()
 
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                binding.edtEmail.error = "Email tidak valid"
+                binding.edtEmail.requestFocus()
+            }
+
+            if (password.isEmpty() || password.length < 6){
+                binding.edtPassoword.error = "Password kurang dari 6"
+                binding.edtPassoword.requestFocus()
+            }
+
+
             if (email.isNotEmpty() || password.isNotEmpty()){
-                login(email, password)
+                if (Patterns.EMAIL_ADDRESS.matcher(email).matches() || password.length > 6) {
+                    login(email, password)
+                }
             } else {
-                Toast.makeText(this,"Tolong di input",Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "Email dan Password belum di isi",Toast.LENGTH_LONG).show()
             }
 
         }
@@ -40,12 +54,15 @@ class MainActivity : AppCompatActivity() {
         firebaseAuth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener(this){task ->
             if (task.isSuccessful){
-                val intent = Intent(this, HomeActivity::class.java)
-                startActivity(intent)
+                Intent(this, HomeActivity::class.java).also {
+                    it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    startActivity(it)
+                }
 
             } else {
-                Toast.makeText(this, "Error",Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "Email atau Password salah",Toast.LENGTH_LONG).show()
             }
         }
+
     }
 }
--- DermaApp/app/src/main/java/com/bangkit/dermaapp/SignUpActivity.kt
+++ DermaApp/app/src/main/java/com/bangkit/dermaapp/SignUpActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.util.Patterns
 import android.widget.Toast
 import com.bangkit.dermaapp.databinding.ActivitySignUpBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -19,14 +20,33 @@ class SignUpActivity : AppCompatActivity() {
         firebaseAuth = FirebaseAuth.getInstance()
 
         binding.btnCreate.setOnClickListener {
+
             val email = binding.edtEmail.text.trim().toString()
             val password = binding.edtPassword.text.trim().toString()
-            if (email.isNotEmpty() || password.isNotEmpty()){
-                createUser(email, password)
-                Toast.makeText(this, "Berhasil daftar", Toast.LENGTH_LONG).show()
-            }else{
-                Toast.makeText(this, "Email dan password tolong di isi :)", Toast.LENGTH_LONG).show()
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                binding.edtEmail.error = "Email tidak valid"
+                binding.edtEmail.requestFocus()
             }
+
+            if (password.isEmpty() || password.length < 6) {
+                binding.edtPassword.error = "Password kurang dari 6"
+                binding.edtPassword.requestFocus()
+            }
+
+
+            if (email.isNotEmpty() || password.isNotEmpty()) {
+                if (Patterns.EMAIL_ADDRESS.matcher(email).matches() || password.length > 6) {
+                    createUser(email, password)
+                }
+            } else {
+                Toast.makeText(this, "Email dan Password belum di isi", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        binding.tvLogin.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 
@@ -34,8 +54,10 @@ class SignUpActivity : AppCompatActivity() {
         firebaseAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this){ task ->
                     if (task.isSuccessful){
-                        val intent = Intent(this, HomeActivity::class.java)
-                        startActivity(intent)
+                        Intent(this, HomeActivity::class.java).also {
+                            it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                            startActivity(it)
+                        }
                         Log.e("TASK MESSAGE", "successful")
                     } else {
                         Log.e("TASK MESSAGE","failed")
