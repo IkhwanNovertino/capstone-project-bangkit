0b87183bc7d9ba19c5837123dff7d315f431ebfa
alzainiazharie <alzainiazharie@gmail.com>
2021-05-16 19:32:00 +0800

    current user & logout & fix bug login

--- DermaApp/app/src/main/java/com/bangkit/dermaapp/HomeActivity.kt
+++ DermaApp/app/src/main/java/com/bangkit/dermaapp/HomeActivity.kt
@@ -1,11 +1,33 @@
 package com.bangkit.dermaapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bangkit.dermaapp.databinding.ActivityHomeBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.btnLogout.setOnClickListener {
+            logout()
+        }
+
+
+    }
+
+    private fun logout(){
+        firebaseAuth.signOut()
+        Intent(this, MainActivity::class.java).also {
+            it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(it)
+        }
     }
 }
--- DermaApp/app/src/main/java/com/bangkit/dermaapp/MainActivity.kt
+++ DermaApp/app/src/main/java/com/bangkit/dermaapp/MainActivity.kt
@@ -1,15 +1,15 @@
 package com.bangkit.dermaapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Patterns
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.bangkit.dermaapp.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,47 +22,54 @@ class MainActivity : AppCompatActivity() {
 
             val email = binding.edtEmail.text.trim().toString()
             val password = binding.edtPassoword.text.trim().toString()
+            val validEmail = Patterns.EMAIL_ADDRESS.matcher(email).matches()
 
-            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            if (email.isEmpty() || !validEmail) {
                 binding.edtEmail.error = "Email tidak valid"
                 binding.edtEmail.requestFocus()
             }
 
-            if (password.isEmpty() || password.length < 6){
+            if (password.isEmpty() || password.length < 6) {
                 binding.edtPassoword.error = "Password kurang dari 6"
                 binding.edtPassoword.requestFocus()
             }
 
-
-            if (email.isNotEmpty() || password.isNotEmpty()){
-                if (Patterns.EMAIL_ADDRESS.matcher(email).matches() || password.length > 6) {
-                    login(email, password)
-                }
-            } else {
-                Toast.makeText(this, "Email dan Password belum di isi",Toast.LENGTH_LONG).show()
+            if (validEmail && password.length > 6) {
+                login(email, password)
             }
 
         }
 
         binding.tvCreateUser.setOnClickListener {
-            val intent = Intent(this,SignUpActivity::class.java)
+            val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
     }
 
-    private fun login(email: String, password: String){
+    private fun login(email: String, password: String) {
         firebaseAuth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this){task ->
-            if (task.isSuccessful){
-                Intent(this, HomeActivity::class.java).also {
-                    it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                    startActivity(it)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Intent(this, HomeActivity::class.java).also {
+                            it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                            startActivity(it)
+                        }
+
+                    } else {
+                        Toast.makeText(this, "Email atau Password salah", Toast.LENGTH_LONG).show()
+                    }
                 }
 
-            } else {
-                Toast.makeText(this, "Email atau Password salah",Toast.LENGTH_LONG).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = firebaseAuth.currentUser
+        if (currentUser != null) {
+            Intent(this, HomeActivity::class.java).also {
+                it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(it)
             }
         }
-
     }
 }
