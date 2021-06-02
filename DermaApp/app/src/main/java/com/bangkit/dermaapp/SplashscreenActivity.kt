35ea71df026b5b9fc822ac56044a7a6118aa25f2
ikhwanNovertino <m.ikhwan.pilliang@gmail.com>
2021-06-02 15:05:43 +0800

    finish splash-screen

--- DermaApp/app/src/main/java/com/bangkit/dermaapp/SplashscreenActivity.kt
+++ DermaApp/app/src/main/java/com/bangkit/dermaapp/SplashscreenActivity.kt
@@ -1,11 +1,22 @@
 package com.bangkit.dermaapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.ActionBar
 
 class SplashscreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
+
+        supportActionBar?.elevation = 0f
+
+        Handler().postDelayed({
+            val intent =Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
